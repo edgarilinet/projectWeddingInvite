17e639ada346c43802e03ae01215b2fb448d91b1
Edgarilinet <krozeedgar@gmail.com>
2023-03-09 23:10:34 +0500

    telegram ajax

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 let chatid = "-1001667496163";
-    let token = "6029601858:AAHZ3Bn0d05uXLswYtrDfyzklrotUxAWo7k";
+let token = "6029601858:AAHZ3Bn0d05uXLswYtrDfyzklrotUxAWo7k";
+window.onload = getMyLocation; 
+let	latitude;
+let	longitude;
+function getMyLocation () {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(displayLocation); 
+	}
+}
+
+function  displayLocation(position) { 
+	latitude = position.coords.latitude; //широтa
+	longitude = position.coords.longitude; //долготa
+	console.log(latitude, longitude)
+}
     
 
 function inform() {
-    let formValue = {
-        family: document.getElementById('family').value,
-        name: document.getElementById('nameFamily').value,
-        budu: document.getElementById('budu').checked,
-        drinks: {
-            wine: document.getElementById('wine').checked,
-            champaine: document.getElementById('spar').checked,
-            vodka: document.getElementById('vodka').checked,
-            wiskey: document.getElementById('wiskey').checked,
-            noAlko: document.getElementById('no-alko').checked,
-            other: document.getElementById('other').checked,
-        }
-    }
-    let text = JSON.stringify(formValue);
-    console.log(JSON.stringify(formValue));
+    let formValue =`
+        Новый ответ:\n
+        от: ${latitude} : ${longitude},\n
+        Фамилия: ${document.getElementById('family').value};\n
+        Имя: ${document.getElementById('nameFamily').value};\n
+        Буду: ${tfTranslate(document.getElementById('budu').checked)}; \n
+        Напитки: \n
+            вино: ${tfTranslate(document.getElementById('wine').checked)};\n
+            Шампанское: ${tfTranslate(document.getElementById('spar').checked)}; \n
+            Водка: ${tfTranslate(document.getElementById('vodka').checked)}; \n
+            Виски: ${tfTranslate(document.getElementById('wiskey').checked)}; \n
+            Без алкоголя: ${tfTranslate(document.getElementById('no-alko').checked)};\n
+            Другое: ${tfTranslate(document.getElementById('other').checked)};\n
+        `
+    let text = formValue;
+    console.log(formValue);
     otpravka(token, text, chatid);
 };
 function otpravka(token, text, chatid) {
@@ -27,4 +42,8 @@ function otpravka(token, text, chatid) {
         data: "parse_mode=HTML&text=" + encodeURIComponent(text),
     });
 };
+function tfTranslate(tf){
+    if (tf) return 'да';
+    else return 'нет';
+}
 
